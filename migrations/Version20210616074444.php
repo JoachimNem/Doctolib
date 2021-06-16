55e6c244a8cf6a473fe5e12611a9ad5331a96ef6
mohamed-bouma <79575387+mohamed-bouma@users.noreply.github.com>
2021-06-16 09:51:46 +0200

    ajout migration table patient et praticien

--- migrations/Version20210616074444.php
+++ migrations/Version20210616074444.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210615085544 extends AbstractMigration
+final class Version20210616074444 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20210615085544 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE patient (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(50) NOT NULL, mdp VARCHAR(255) NOT NULL, nom VARCHAR(50) NOT NULL, prenom VARCHAR(50) NOT NULL, telephone INT NOT NULL, date_naissance DATE NOT NULL, adresse_num INT NOT NULL, adresse_rue VARCHAR(100) NOT NULL, adresse_cp INT NOT NULL, adresse_ville VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE praticien (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, mdp VARCHAR(255) NOT NULL, nom VARCHAR(50) NOT NULL, prenom VARCHAR(50) DEFAULT NULL, specialite VARCHAR(50) NOT NULL, telephone INT NOT NULL, adresse_num VARCHAR(25) NOT NULL, adresse_rue VARCHAR(50) NOT NULL, adresse_cp INT NOT NULL, adresse_ville VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE patient');
         $this->addSql('DROP TABLE praticien');
     }
 }
