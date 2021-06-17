ec4b91d88a84c29a08550b3c8d0eecf5949bc817
Joachim Nemiche <nemiche.joachim@gmail.com>
2021-06-17 16:28:45 +0200

    Tests Repository Rdv

--- tests/Repository/RdvRepositoryTest.php
+++ tests/Repository/RdvRepositoryTest.php
@@ -1,24 +1,58 @@
 <?php
 
-// namespace App\Tests\Repository;
+namespace App\Tests\Repository;
 
-// use App\DataFixtures\RdvFixtures;
-// use App\Repository\PraticienRepository;
-// use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-// use Liip\TestFixturesBundle\Test\FixturesTrait;
+use App\DataFixtures\RdvFixtures;
+use App\Repository\RdvRepository;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
 
-// class RdvRepositoryTest extends KernelTestCase
-// {
+class RdvRepositoryTest extends KernelTestCase
+{
 
-//     use FixturesTrait;
+    use FixturesTrait;
 
-//     public function testFindAll()
-//     {
-//         self::bootKernel();
-//         $repository = self::$container->get(PraticienRepository::class);
-//         $this->loadFixtures([PraticienFixtures::class]);
-//         $rdvs = $repository->findAll();
+    public function testFindAll()
+    {
+        self::bootKernel();
+        $repository = self::$container->get(RdvRepository::class);
+        $this->loadFixtures([RdvFixtures::class]);
+        $rdvs = $repository->findAll();
 
-//         $this->assertCount(3, $rdvs);
-//     }
-// }
+        $this->assertCount(3, $rdvs);
+    }
+
+    public function testFind()
+    {
+        self::bootKernel();
+        $repository = self::$container->get(RdvRepository::class);
+        $this->loadFixtures([RdvFixtures::class]);
+        $rdvs = $repository->findAll();
+        $result = $repository->find($rdvs[0]->getId());
+
+        $this->assertEquals($rdvs[0]->getId(), $result->getId());
+    }
+
+    public function testFindOneBy()
+    {
+        self::bootKernel();
+        $repository = self::$container->get(RdvRepository::class);
+        $this->loadFixtures([RdvFixtures::class]);
+        $rdvs = $repository->findAll();
+        $tab["date"] = $rdvs[0]->getDate();
+        $result = $repository->findOneBy($tab);
+
+        $this->assertEquals($rdvs[0]->getDate(), $result->getDate());
+    }
+
+    public function testFindBy()
+    {
+        self::bootKernel();
+        $repository = self::$container->get(RdvRepository::class);
+        $this->loadFixtures([RdvFixtures::class]);
+        $tab["id"] = 2;
+        $rdvs = $repository->findBy($tab);
+
+        $this->assertCount(1, $rdvs);
+    }
+}
