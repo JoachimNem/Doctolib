c77fd30761c412ddbf1b2a3ee079aad74e683ee5
Joachim Nemiche <nemiche.joachim@gmail.com>
2021-06-17 10:26:55 +0200

    tests unitaires praticien / patient / rdv

--- tests/Entity/PatientTest.php
+++ tests/Entity/PatientTest.php
@@ -6,6 +6,8 @@ use PHPUnit\Framework\TestCase;
 use App\Entity\Patient;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
+use function PHPUnit\Framework\assertEquals;
+
 class PatientTest extends KernelTestCase
 {
 
@@ -51,5 +53,68 @@ class PatientTest extends KernelTestCase
         $this->assertCount(0, $errors, "Une erreur est attendue car plus de 3 chars");
         $this->assertEquals(0, count($errors), "Une erreur est attendue car plus de 3 chars");
         // $this->assertEquals("Your name must be at least 3 characters long", $errors[0]->getMessage());
+    }
+    public function testSetGetPrenom()
+    {
+        $patient = new Patient;
+        $patient->setPrenom("Charles");
+        $prenom = $patient->getPrenom();
+
+        $this->assertEquals("Charles", $prenom);
+    }
+
+    public function testSetGetDateNaissance()
+    {
+        $patient = new Patient;
+        $date = new \DateTime;
+        $patient->setDateNaissance($date);
+        $date_naissance = $patient->getDateNaissance();
+
+        $this->assertEquals($date, $date_naissance);
+    }
+
+    public function testSetGetTelephone()
+    {
+        $patient = new Patient();
+        $patient->setTelephone(0606060606);
+        $telephone = $patient->getTelephone();
+
+        assertEquals(0606060606, $telephone);
+    }
+
+    public function testSetGetAdresse_num()
+    {
+        $patient = new Patient();
+        $patient->setAdresseNum("5 bis");
+        $adresse_num = $patient->getAdresseNum();
+
+        assertEquals("5 bis", $adresse_num);
+    }
+
+    public function testSetGetAdresse_rue()
+    {
+        $patient = new Patient();
+        $patient->setAdresseRue("rue de la paix");
+        $adresse_rue = $patient->getAdresseRue();
+
+        assertEquals("rue de la paix", $adresse_rue);
+    }
+
+    public function testSetGetAdresse_cp()
+    {
+        $patient = new Patient();
+        $patient->setAdresseCp(59000);
+        $adresse_cp = $patient->getAdresseCp();
+
+        assertEquals(59000, $adresse_cp);
+    }
+
+    public function testSetGetAdresse_ville()
+    {
+        $patient = new Patient();
+        $patient->setAdresseVille("RBX");
+        $adresse_ville = $patient->getAdresseVille();
+
+        assertEquals("RBX", $adresse_ville);
     }
 }
--- tests/Entity/PraticienTest.php
+++ tests/Entity/PraticienTest.php
@@ -4,6 +4,7 @@ use PHPUnit\Framework\TestCase;
 use App\Entity\Praticien;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
+use function PHPUnit\Framework\assertEquals;
 
 class PraticientTest extends KernelTestCase
 {
@@ -24,6 +25,7 @@ class PraticientTest extends KernelTestCase
 
         $this->assertEquals("DUPOND", $nom, "getNom returns a bad value.");
     }
+
     public function testNomIsInvalid()
     {
         $kernel = self::bootKernel();
@@ -48,5 +50,68 @@ class PraticientTest extends KernelTestCase
         $this->assertCount(0, $errors, "Une erreur est attendue car plus de 3 chars");
         $this->assertEquals(0, count($errors), "Une erreur est attendue car plus de 3 chars");
         // $this->assertEquals("Your name must be at least 3 characters long", $errors[0]->getMessage());
+    }
+
+    public function testSetGetPrenom()
+    {
+        $praticien = new Praticien;
+        $praticien->setPrenom("Charles");
+        $prenom = $praticien->getPrenom();
+
+        $this->assertEquals("Charles", $prenom);
+    }
+
+    public function testSetGetSpecialite()
+    {
+        $praticien = new Praticien;
+        $praticien->setSpecialite("Generaliste");
+        $specialite = $praticien->getSpecialite();
+
+        $this->assertEquals("Generaliste", $specialite);
+    }
+
+    public function testSetGetTelephone()
+    {
+        $praticien = new Praticien();
+        $praticien->setTelephone(0606060606);
+        $telephone = $praticien->getTelephone();
+
+        assertEquals(0606060606, $telephone);
+    }
+
+    public function testSetGetAdresse_num()
+    {
+        $praticien = new Praticien();
+        $praticien->setAdresseNum("5 bis");
+        $adresse_num = $praticien->getAdresseNum();
+
+        assertEquals("5 bis", $adresse_num);
+    }
+
+    public function testSetGetAdresse_rue()
+    {
+        $praticien = new Praticien();
+        $praticien->setAdresseRue("rue de la paix");
+        $adresse_rue = $praticien->getAdresseRue();
+
+        assertEquals("rue de la paix", $adresse_rue);
+    }
+
+    public function testSetGetAdresse_cp()
+    {
+        $praticien = new Praticien();
+        $praticien->setAdresseCp(59000);
+        $adresse_cp = $praticien->getAdresseCp();
+
+        assertEquals(59000, $adresse_cp);
+    }
+
+    public function testSetGetAdresse_ville()
+    {
+        $praticien = new Praticien();
+        $praticien->setAdresseVille("RBX");
+        $adresse_ville = $praticien->getAdresseVille();
+
+        assertEquals("RBX", $adresse_ville);
     }
 }
